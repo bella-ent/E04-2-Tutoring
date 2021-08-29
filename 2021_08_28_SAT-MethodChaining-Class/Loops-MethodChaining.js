1c2ed3b15c1f30149b6d101bb8c3dbe056688ad0
Bella Yang <bellalucajosef@gmail.com>
2021-08-29 10:57:21 +0200

    changed Files Names

--- 2021_08_28_SAT-MethodChaining-Class/Loops-MethodChaining.js
+++ 2021_08_28_SAT-MethodChaining-Class/Loops-MethodChaining.js
@@ -32,116 +32,120 @@ const people = [
 	},
 ];
 
-//? How can we get the value of specific elements' key
+//? How can we get the value of a specific element's key
 // console.log(people[0].name); //the value of a key property in the first element
 // console.log(people[4].name); //the value of a key property in the last element
-// console.log(people[people.length-1].name);  // Another way: How to get the value of a key property in the last element. 
+// console.log(people[people.length-1].name);  // Another way: How to get the value of a key property in the last element.
 // array[array.length -1]: It is a common way to get the last element of an array in the case of unknowing the last element's index.
-
 
 //! To get all name properties' values from all elements of array and put them into an new array.
 //? For Loop
 
 const forLoop = (array) => {
-    let resultArray = [];
-    for(let i =0;i<array.length; i++) {
-        resultArray.push(array[i].name)
-    }
-    return resultArray
-}
+	let resultArray = [];
+	for (let i = 0; i < array.length; i++) {
+		resultArray.push(array[i].name);
+	}
+	return resultArray;
+};
 // console.log(forLoop(people));
 
 //? while loop
 
 const whileLoop = (array) => {
-    let resultArray = [];
-    let counter = 0;
-    while(counter < array.length) {
-        resultArray.push(array[counter].name);
-        counter++;
-    }
-    return resultArray
-}
+	let resultArray = [];
+	let counter = 0;
+	while (counter < array.length) {
+		resultArray.push(array[counter].name);
+		counter++;
+	}
+	return resultArray;
+};
 // console.log(whileLoop(people));
 
 //? do-while loop
 
 const doWhileLoop = (array) => {
-    let resultArray = [];
-    let counter = 0;
-    do {
-        resultArray.push(array[counter].name);
-        counter=counter+1;
-        // counter+=1
-        // counter++
-    }while(counter<=array.length-1);
-    return resultArray;
-}
+	let resultArray = [];
+	let counter = 0;
+	do {
+		resultArray.push(array[counter].name);
+		counter = counter + 1;
+		// counter+=1
+		// counter++
+	} while (counter <= array.length - 1);
+	return resultArray;
+};
 // console.log(doWhileLoop(people));
 
 //? for ... of (for arrays)
 
 const forOf = (array) => {
-    const resultArray = [];
-    for (const element of array) {
-        resultArray.push(element.name);
-    }
-    return resultArray;
-}
+	const resultArray = [];
+	for (const element of array) {
+		resultArray.push(element.name);
+	}
+	return resultArray;
+};
 // console.log(forOf(people));
 
 //* Advanced array methods (All advanced array methods need a call-back function as an argument)
 //? forEach method
 
 const forEach = (array) => {
-    const resultArray = [];
-    array.forEach((el)=> {
-       return resultArray.push(el.name);
-    })
-    return resultArray;
-}
+	const resultArray = [];
+	array.forEach((el) => {
+		return resultArray.push(el.name);
+	});
+	return resultArray;
+};
 console.log(forEach(people));
 
 //? map method
 const map = (array) => {
-   return array.map((el)=>{
-        return el.name
-    })
-}
+	return array.map((el) => {
+		return el.name;
+	});
+};
 console.log(map(people));
 
-const map1 = people.map((el) => el.name)
+const map1 = people.map((el) => el.name);
 console.log(map1);
 
 //? filter method
 
-const filter1 = people.filter((el)=> el.name.length>=5 )
+const filter1 = people.filter((el) => el.name.length >= 5);
 console.log(filter1);
-
 
 //! To get only names in new array who lives in Berlin using the method chaining.
 
-const getNamesInCity = people.filter((el)=> el.city === "Berlin").map((el)=>el.name
-)
+const getNamesInCity = people
+	.filter((el) => el.city === "Berlin")
+	.map((el) => el.name);
 console.log(getNamesInCity);
 
-
 //! To get names whose hobby are a gardening.
-const getNamesIncludeGardening = people.filter((el)=> el.hobby.includes("gardening")).map((el)=>el.name)
+const getNamesIncludeGardening = people
+	.filter((el) => el.hobby.includes("gardening"))
+	.map((el) => el.name);
 console.log(getNamesIncludeGardening);
-
 
 //* Destructuring ver. for the previous one.
 {
-    const getNamesIncludeGardening = people.filter(({hobby})=> hobby.includes("gardening")).map(({name})=>name)
-console.log(getNamesIncludeGardening);
+	const getNamesIncludeGardening = people
+		.filter(({ hobby }) => hobby.includes("gardening"))
+		.map(({ name }) => name);
+	console.log(getNamesIncludeGardening);
 }
 
 //! To get only names in new array who lives in Berlin using the method chaining and sort the result in alphabet order.
-const sortedGetNamesInCity = people.filter((el)=> el.city === "Berlin").map((el)=>el.name).sort()
+const sortedGetNamesInCity = people
+	.filter((el) => el.city === "Berlin")
+	.map((el) => el.name)
+	.sort();
 console.log(sortedGetNamesInCity);
 
 //* Another way to sort the result in alphabet order. (using localeCompare)
-const numbers = ["Banana","Apple","Orange","Pear"]
-const sortedNumbers = numbers.sort((a,b)=> b.localeCompare(a))
+const numbers = ["Banana", "Apple", "Orange", "Pear"];
+const sortedNumbers = numbers.sort((a, b) => b.localeCompare(a));
 console.log(sortedNumbers);
